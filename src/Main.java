679478732d00aa904ca99dd82064289b961158be
leafbug16 <leafbug16@gmail.com>
2023-12-17 11:47:18 +0900

    [13223] 소금 폭탄 나의 풀이(틀림)

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String doc = sc.nextLine();
-        String word = sc.nextLine();
+        String now = sc.nextLine();
+        String salt = sc.nextLine();
+        String[] dividedNow = now.split(":");
+        String[] dividedSalt = salt.split(":");
+        int[] intNow = new int[dividedNow.length];
+        int[] intSalt = new int[dividedSalt.length];
+        int resHour = 0;
+        int resMinute = 0;
+        int resSecond = 0;
 
-        String replaced = doc.replace(word, "");
-        int res = (doc.length() - replaced.length()) / word.length();
-        System.out.println(res);
-        System.out.println("되돌린 후 다시 push");
+        for (int i=0; i<dividedNow.length; i++) {
+            intNow[i] = Integer.parseInt(dividedNow[i]);
+            intSalt[i] = Integer.parseInt(dividedSalt[i]);
+        }
+
+        if (intSalt[0] - intNow[0] < 0) {
+            resHour = (24 - intNow[0] + intSalt[0]);
+        } else {
+            resHour = (intSalt[0] - intNow[0]);
+        }
+
+        if (intSalt[1] - intNow[1] < 0) {
+            resHour--;
+            resMinute = (60 - intNow[1] + intSalt[1]);
+        } else {
+            resMinute = (intSalt[1] - intNow[1]);
+        }
+
+        if (intSalt[2] - intNow[2] < 0) {
+            resMinute--;
+            resSecond = (60 - intNow[2] + intSalt[2]);
+        } else {
+            resMinute = (intSalt[2] - intNow[2]);
+        }
+
+        System.out.printf("%02d:%02d:%02d", resHour, resMinute, resSecond);
     }
 }
