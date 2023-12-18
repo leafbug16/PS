3f60137383ef1213e80930cb419de0ffad3a0c09
leafbug16 <leafbug16@gmail.com>
2023-12-18 15:55:54 +0900

    [2577]

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        sc.close();
-
-        StringTokenizer st = new StringTokenizer(a," ");
-        System.out.println(st.countTokens());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String multiple = a * b * c + "";
+        int[] count = new int[10];
+        for (int i=0; i<multiple.length(); i++) {
+            count[multiple.charAt(i) - '0']++;
+        }
+        for (int i=0; i<count.length; i++) {
+            System.out.println(count[i]);
+        }
 
     }
 }
