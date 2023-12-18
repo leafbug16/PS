5bf8556c946b18127e961f9a2caf44c7c2fef631
leafbug16 <leafbug16@gmail.com>
2023-12-18 15:44:54 +0900

    [1152]

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] a = sc.nextLine().split(" ");
-        int[] b = new int[5];
-        int tmp = 0;
-        for (int i=0; i<a.length; i++) {
-            b[i] = Integer.parseInt(a[i]) * Integer.parseInt(a[i]);
-            tmp += b[i];
-        }
-        System.out.println(tmp%10);
+        String a = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(a," ");
+        System.out.println(st.countTokens());
+
     }
 }
