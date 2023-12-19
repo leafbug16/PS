24dcfaeb2e1f5378ea051a55ffaa0156c8074966
leafbug16 <leafbug16@gmail.com>
2023-12-19 19:56:03 +0900

    [3052]

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        String multiple = a * b * c + "";
-        int[] count = new int[10];
-        for (int i=0; i<multiple.length(); i++) {
-            count[multiple.charAt(i) - '0']++;
-        }
-        for (int i=0; i<count.length; i++) {
-            System.out.println(count[i]);
+        int[] arr = new int[10];
+        for (int i=0; i<10; i++) {
+            int tmp = sc.nextInt()%42;
+            arr[i] = tmp;
         }
 
+        int cnt = 10;
+
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                if (i != j && arr[i] != -1 && arr[j] != -1) {
+                    if (arr[i] == arr[j]) {
+                        arr[j] = -1;
+                        cnt--;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
