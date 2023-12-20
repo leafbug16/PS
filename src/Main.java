13d6adab3c08d0d24c3c42cc13145a254b5d7e7c
leafbug16 <leafbug16@gmail.com>
2023-12-20 17:51:23 +0900

    [10250]

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[8];
-        for (int i=0; i<8; i++) {
-            num[i] = sc.nextInt();
-        }
-        String res = "";
-        for (int i=0; i<7; i++) {
-            if (num[i] - num[i+1] != 1 && num[i] - num[i+1] != -1) {
-                res = "mixed";
-                break;
-            } else if (num[i] - num[i+1] == 1) {
-                res = "descending";
+        int T = sc.nextInt();
+        for (int i=0; i<T; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+
+            int a = N % H;
+            int b = N / H;
+            if (a == 0) {
+                System.out.printf("%d%02d\n", H, b);
             } else {
-                res = "ascending";
+                System.out.printf("%d%02d\n", a, b+1);
             }
         }
-        System.out.println(res);
+
     }
 }
