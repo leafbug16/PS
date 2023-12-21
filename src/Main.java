21447e48993c1cd8f976862060a0e00666ce6cb4
leafbug16 <leafbug16@gmail.com>
2023-12-21 16:49:41 +0900

    [15964]

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i=0; i<T; i++) {
-            int H = sc.nextInt();
-            int W = sc.nextInt();
-            int N = sc.nextInt();
-
-            int a = N % H;
-            int b = N / H;
-            if (a == 0) {
-                System.out.printf("%d%02d\n", H, b);
-            } else {
-                System.out.printf("%d%02d\n", a, b+1);
-            }
-        }
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println((a+b)*(a-b));
 
     }
 }
