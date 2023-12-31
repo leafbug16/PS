3a312da99d58f632ba2c9999f83ee713931d844d
leafbug16 <147241132+leafbug16@users.noreply.github.com>
2023-12-31 18:03:09 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 964 ms, Memory: 299020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String[] nums = br.readLine().split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            bw.write(a + b + "\n");
+        }
+        bw.flush();
+    }
+}
